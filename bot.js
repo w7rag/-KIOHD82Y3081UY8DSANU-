003293d6c36e73c78c60f98b882d10218bd3ca8d
w7rag <ytsilverbusiness@gmail.com>
2024-09-22 13:30:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,17 @@ const client = new Client({
 });
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running and alive!'); 
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const productsFile = path.join(__dirname, 'products.json');
 
